738c0f5daebc2971bb86c93b17b0571a62bc097f
tayurus <tayurus@gmail.com>
2018-12-05 20:40:37 +0300

    add player

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
 
-var speaker = document.querySelector('.speaker');
 
-speaker.onclick = function(){
+  var speaker = document.querySelector('.speaker');
   var video = document.querySelector(".enter__video");
-  if (video.muted){
-    video.muted= false;
+
+  // video.trigger()
+
+  speaker.onclick = function() {
+    if (video.muted) {
+      video.muted = false;
+    } else {
+      video.muted = true;
+    }
   }
-  else {
-    video.muted= true;
+
+  var player = document.querySelector('.player');
+
+  player.onclick = function() {
+
+    var video = document.querySelector(".enter__video");
+    var picture = document.querySelector(".enter__picture");
+
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+    video.classList.toggle('hidden');
+    picture.classList.toggle('hidden');
+
   }
-}
+})
